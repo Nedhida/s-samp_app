6f44523d9db59067d7bed6c8185dd83659af1f38
EC2 Default User <ec2-user@ip-172-31-20-19.ec2.internal>
2021-11-17 23:35:42 +0000

    [Finish]3-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'top' => 'homes#top'
-
+#「.../todolists」というURLにpostメソッドでリクエストした時、todolistsコントローラーのcreateアクションが呼び出される
+post 'todolists' => 'todolists#create'
 end
 
